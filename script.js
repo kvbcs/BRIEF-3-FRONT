8af375870b4bcecf29318232d1394b560288d28d
Kyllian <kyllianvibancos2@gmail.com>
2024-04-04 12:26:30 +0200

    modified design a bit

--- script.js
+++ script.js
@@ -1,8 +1,41 @@
 console.log("hello");
 
-let body = document.querySelector("body");
-let header = document.querySelector("header");
-let footer = document.querySelector("footer");
+async function handleRegister(event) {
+	event.preventDefault();
+
+	let firstName = document.querySelector(".firstName").value;
+	let lastName = document.querySelector(".lastName").value;
+	let email = document.querySelector(".email").value;
+	let password = document.querySelector(".password").value;
+
+	let newUser = {
+		firstName: firstName,
+		lastName: lastName,
+		email: email,
+		password: password,
+	};
+
+	let request = {
+		method: "POST",
+		headers: {
+			"Content-Type": "application/json; charset=utf-8",
+		},
+		body: JSON.stringify(newUser),
+	};
+	try {
+		let apiRequest = await fetch(
+			"http://localhost:3000/user/register",
+			request
+		);
+		let response = await apiRequest.json();
+		console.log(response);
+		if (response.status === 200) {
+			window.location.href = "index.html";
+		}
+	} catch (e) {
+		console.log(e);
+	}
+}
 
 class Annonce {
 	constructor(id, titre, image, description, iduser) {
